82bf8eae818ec17c72ab67c4dd20afb652e70920
Bondarenko-In-M <innabondarenkowork@gmail.com>
2026-09-20 20:25:55 +0300

    Refactor log_error procedure for consistency

--- PLSQL/Procedures/log_error_процедура.sql
+++ PLSQL/Procedures/log_error_процедура.sql
@@ -9,7 +9,7 @@ BEGIN
   
     IF p_text IS NULL THEN
       
-       v_text  :=''В процедурі' || p_proc_name ||' сталася помилка. ' || p_sglerrm;
+       v_text  :=''В процедурі' || p_proc_name ||' сталася помилка. ' || p_sqlerrm;
     ELSE
         v_text := p_text;
 
