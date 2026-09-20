f75b044ebee0314b91a8ef236f9fe125b939d347
Bondarenko-In-M <innabondarenkowork@gmail.com>
2026-09-20 19:14:19 +0300

    Add files via upload

---
+++ PLSQL/Scripts/log_start виклик.sql
@@ -0,0 +1,12 @@
+
+DECLARE
+    v_text VARCHAR2(4000);
+BEGIN
+    
+    log_start (p_proc_name => 'TEST_PROC',
+               p_text      => NULL);
+                                   
+    dbms_output.put_line (v_text); 
+
+END;
+/
