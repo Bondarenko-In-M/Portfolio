fc21cad81149d6234262323a5525bf8a6ba0ce84
Bondarenko-In-M <innabondarenkowork@gmail.com>
2026-09-21 08:36:27 +0300

    Change default value for p_text parameter in log_error

--- PLSQL/Procedures/log_error_процедура.sql
+++ PLSQL/Procedures/log_error_процедура.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE log_error(p_proc_name IN VARCHAR2,
-                                      p_text      IN VARCHAR2 := NULL,
+                                      p_text      IN VARCHAR2 DEFAULT NULL,
                                       p_sqlerrm   IN VARCHAR2) is
                                        
        v_text VARCHAR2(4000);
